f5ed1df5fb9958565f5173fbbbea785b7c4343f5
Victor Medeiros <victoramedeiros@gmail.com>
2024-03-05 16:30:06 -0500

    cleanup

--- a_core/settings.py
+++ a_core/settings.py
@@ -49,7 +49,11 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-NPM_BIN_PATH = "/home/victor/.nvm/versions/node/v21.6.2/bin/node"
+# For Win config
+NPM_BIN_PATH = "C:\\Program Files\\nodejs\\npm.cmd"
+
+# For linux/macos configs
+# NPM_BIN_PATH = "/home/victor/.nvm/versions/node/v21.6.2/bin/node"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
